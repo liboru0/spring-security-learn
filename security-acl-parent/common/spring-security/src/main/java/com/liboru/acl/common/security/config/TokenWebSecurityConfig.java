0bc36c1aa1d51eaf74dafbcede3dfb666a56e85f
liboru <1036513264@qq.com>
2021-04-20 16:03:27 +0800

    security 核心配置类

---
+++ security-acl-parent/common/spring-security/src/main/java/com/liboru/acl/common/security/config/TokenWebSecurityConfig.java
@@ -0,0 +1,68 @@
+package com.liboru.acl.common.security.config;
+
+import com.liboru.acl.common.security.filter.TokenAuthFilter;
+import com.liboru.acl.common.security.filter.TokenLoginFilter;
+import com.liboru.acl.common.security.security.DefaultPasswordEncoder;
+import com.liboru.acl.common.security.security.TokenLogoutHandler;
+import com.liboru.acl.common.security.security.TokenManager;
+import com.liboru.acl.common.security.security.UnAuthEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class TokenWebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private TokenManager tokenManager;
+    private RedisTemplate redisTemplate;
+    private DefaultPasswordEncoder defaultPasswordEncoder;
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public TokenWebSecurityConfig(TokenManager tokenManager, RedisTemplate redisTemplate
+            , DefaultPasswordEncoder defaultPasswordEncoder, UserDetailsService userDetailsService) {
+        this.tokenManager = tokenManager;
+        this.redisTemplate = redisTemplate;
+        this.defaultPasswordEncoder = defaultPasswordEncoder;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.exceptionHandling()
+                .authenticationEntryPoint(new UnAuthEntryPoint())
+                .and().csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and().logout().logoutUrl("/admin/acl/index/logout")
+                .addLogoutHandler(new TokenLogoutHandler(tokenManager, redisTemplate)).and()
+                .addFilter(new TokenLoginFilter(authenticationManager(), tokenManager, redisTemplate))
+                .addFilter(new TokenAuthFilter(authenticationManager(), tokenManager, redisTemplate)).httpBasic();
+    }
+
+    /**
+     * 密码处理
+     */
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(defaultPasswordEncoder);
+    }
+
+    /**
+     * 配置哪些请求不认证
+     */
+    @Override
+    public void configure(WebSecurity web)  throws Exception {
+        web.ignoring().antMatchers( "/api/**" ,  "/swagger-ui.html/**");
+    }
+
+}
