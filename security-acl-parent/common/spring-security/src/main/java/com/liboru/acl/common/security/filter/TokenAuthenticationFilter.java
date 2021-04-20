30c289f02f5fcacf4675e86e2feca724bcc84350
liboru <1036513264@qq.com>
2021-04-20 12:46:57 +0800

    Revert "test"
    This reverts commit 1227a27f

--- security-acl-parent/common/spring-security/src/main/java/com/liboru/acl/common/security/filter/TokenAuthenticationFilter.java
+++
@@ -1,4 +0,0 @@
-package com.liboru.acl.common.security.filter;
-
-public class TokenAuthenticationFilter {
-}
--- security-acl-parent/common/spring-security/src/main/java/com/liboru/acl/common/security/filter/TokenLoginFilter.java
+++
@@ -1,8 +0,0 @@
-package com.liboru.acl.common.security.filter;
-
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-public class TokenLoginFilter extends UsernamePasswordAuthenticationFilter {
-
-
-}
